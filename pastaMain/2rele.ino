55656d2da8b9792a01a354059adc9463b002cf0a
PedrooGit <pedroogit@gmail.com>
2026-05-26 21:36:04 -0300

    arrumei a furmatacao

--- pastaMain/2rele.ino
+++ pastaMain/2rele.ino
@@ -4,19 +4,21 @@
 #define rele_esq 12
 
 void setup() {
-  pinMode(rele_dir, OUTPUT);
-  pinMode(rele_esq, OUTPUT);
+    
+    pinMode(rele_dir, OUTPUT);
+    pinMode(rele_esq, OUTPUT);
 }
 
 
 void loop() {
-  digitalWrite(rele_dir, LOW);  
-  digitalWrite(rele_esq, HIGH);
-  delay(3000);
+
+    digitalWrite(rele_dir, LOW);    
+    digitalWrite(rele_esq, HIGH);
+    delay(3000);
 
 
-  digitalWrite(rele_dir, HIGH);  
-  digitalWrite(rele_esq, LOW);
-  delay(3000);
+    digitalWrite(rele_dir, HIGH);  
+    digitalWrite(rele_esq, LOW);
+    delay(3000);
 }
 
