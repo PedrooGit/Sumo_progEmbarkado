9e145a571e3363d812a260d12a14523a481ef050
PedrooGit <pedroogit@gmail.com>
2026-05-26 22:08:08 -0300

    cod que junta 3sensor +2rele

---
+++ pastaMain/direitaEesqurda.ino
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+
+#define trig_1 7
+#define echo_1 6
+#define trig_2 8
+#define echo_2 9
+#define trig_3 4
+#define echo_3 3
+
+#define rele_dir 13
+#define rele_esq 12
+
+float distancia_1;
+float distancia_2;
+float distancia_3;
+
+void setup() {
+
+    pinMode(trig_1, OUTPUT);
+    pinMode(echo_1, INPUT);
+    pinMode(trig_2, OUTPUT);
+    pinMode(echo_2, INPUT);
+    pinMode(trig_3, OUTPUT);
+    pinMode(echo_3, INPUT);
+
+    pinMode(rele_dir, OUTPUT);
+    pinMode(rele_esq, OUTPUT);
+
+    Serial.begin(9600);
+}
+
+void loop() {
+
+    distancia_1 = sensorUltra(trig_1, echo_1);
+    distancia_2 = sensorUltra(trig_2, echo_2);
+    distancia_3 = sensorUltra(trig_3, echo_3);
+
+    if (distancia_1 >= 100){
+
+        if(distancia_2 <= 50){
+            digitalWrite(rele_dir, LOW);    
+            digitalWrite(rele_esq, HIGH);
+        }else if(distancia_3 <= 50){ 
+            digitalWrite(rele_dir, HIGH);  
+            digitalWrite(rele_esq, LOW);
+        }else{
+            digitalWrite(rele_dir, HIGH);
+            digitalWrite(rele_esq, HIGH);            
+        }
+    }
+
+}
+
+float sensorUltra(int trig, int echo) {
+
+    digitalWrite(trig, LOW);
+    delayMicroseconds(5);
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW);
+
+    float distancia = pulseIn(echo, HIGH);
+    distancia = distancia / 58.0;
+
+    return distancia;
+}
